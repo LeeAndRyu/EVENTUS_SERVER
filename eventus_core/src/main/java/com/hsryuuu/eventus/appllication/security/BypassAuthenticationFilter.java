574388c37bd913674b62db4336db35fff9a26ef2
happyhsryu <happyhsryu@gmail.com>
2025-06-28 14:26:34 +0900

    Feat: Bypass 유저 팩토리 - 테스트 용이성을 위함

--- eventus_core/src/main/java/com/hsryuuu/eventus/appllication/security/BypassAuthenticationFilter.java
+++ eventus_core/src/main/java/com/hsryuuu/eventus/appllication/security/BypassAuthenticationFilter.java
@@ -1,24 +1,14 @@
 package com.hsryuuu.eventus.appllication.security;
 
-import com.hsryuuu.eventus.appllication.dev.constants.DevConstants;
-import com.hsryuuu.eventus.appllication.security.principal.AppUserPrincipal;
-import com.hsryuuu.eventus.appllication.security.principal.AuthPrincipal;
-import com.hsryuuu.eventus.user.appuser.UserRole;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 public class BypassAuthenticationFilter extends OncePerRequestFilter {
 
@@ -31,33 +21,10 @@ public class BypassAuthenticationFilter extends OncePerRequestFilter {
 
         // 이미 인증된 사용자가 없을 경우만 주입
         if (SecurityContextHolder.getContext().getAuthentication() == null) {
-            this.setAllAuthorizedUser();
+            Authentication authentication = BypassUserFactory.getAllAuthorizedAuthentication();
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    private void setAllAuthorizedUser() {
-        List<GrantedAuthority> allUserAuthorities = this.getAllUserAuthorities();
-
-        AuthPrincipal principal = new AppUserPrincipal(
-                UUID.fromString(DevConstants.SUPER_USER_ID),
-                DevConstants.ADMIN_USER_NAME,
-                UserRole.ADMIN,
-                allUserAuthorities);
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-                principal, null, principal.getAuthorities()
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
-
-    private List<GrantedAuthority> getAllUserAuthorities() {
-        List<GrantedAuthority> allAuthorities = new ArrayList<>();
-        for (UserRole value : UserRole.values()) {
-            allAuthorities.add(new SimpleGrantedAuthority("ROLE_" + value.name()));
-        }
-        return allAuthorities;
     }
 }
---
+++ eventus_core/src/main/java/com/hsryuuu/eventus/appllication/security/BypassUserFactory.java
@@ -0,0 +1,39 @@
+package com.hsryuuu.eventus.appllication.security;
+
+import com.hsryuuu.eventus.appllication.dev.constants.DevConstants;
+import com.hsryuuu.eventus.appllication.security.principal.AppUserPrincipal;
+import com.hsryuuu.eventus.appllication.security.principal.AuthPrincipal;
+import com.hsryuuu.eventus.user.appuser.UserRole;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class BypassUserFactory {
+
+    public static Authentication getAllAuthorizedAuthentication() {
+        List<GrantedAuthority> allUserAuthorities = getAllUserAuthorities();
+
+        AuthPrincipal principal = new AppUserPrincipal(
+                UUID.fromString(DevConstants.SUPER_USER_ID),
+                DevConstants.ADMIN_USER_NAME,
+                UserRole.ADMIN,
+                allUserAuthorities);
+
+        return new UsernamePasswordAuthenticationToken(
+                principal, null, principal.getAuthorities()
+        );
+    }
+
+    public static List<GrantedAuthority> getAllUserAuthorities() {
+        List<GrantedAuthority> allAuthorities = new ArrayList<>();
+        for (UserRole value : UserRole.values()) {
+            allAuthorities.add(new SimpleGrantedAuthority("ROLE_" + value.name()));
+        }
+        return allAuthorities;
+    }
+}
