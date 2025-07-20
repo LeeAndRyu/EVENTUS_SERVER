60db2271eb88b83c8e0985921886b437c7273fa3
happyhsryu <happyhsryu@gmail.com>
2025-07-20 14:06:33 +0900

    Chore: flyway 테이블 정의 수정

--- eventus_core/src/main/resources/db/migration/V0.0.3__insert_default_users.sql
+++ eventus_core/src/main/resources/db/migration/V0.0.3__insert_default_users.sql
@@ -8,11 +8,11 @@ insert into app_user (id,
                       created_at,
                       last_modified_at)
 values ('00000000-0000-0000-0000-000000000001',
-        'super-user@example.com',
-        'SuperUser',
+        'root@example.com',
+        'root',
         '010-0000-0000',
-        'password123', -- 실제 사용 시 암호화 필요
+        'root', -- 실제 사용 시 암호화 필요
         'ADMIN',
-        'SuperUser',
+        'root',
         now(),
         now());
--- eventus_core/src/main/resources/db/migration/V0.0.4__task_ddl.sql
+++ eventus_core/src/main/resources/db/migration/V0.0.4__task_ddl.sql
@@ -1,19 +1,46 @@
-CREATE TABLE task
+CREATE TABLE category
 (
     id         UUID PRIMARY KEY,
-    title      VARCHAR(255) NOT NULL,
-    type       VARCHAR(20)  NOT NULL,
-    status     VARCHAR(20)  NOT NULL,
-    start_at   TIMESTAMP,
-    due_at     TIMESTAMP,
-    priority   VARCHAR(20),
+    name       VARCHAR(255) NOT NULL,
+    creator_id UUID         NOT NULL,
     created_at TIMESTAMP    NOT NULL DEFAULT now(),
     updated_at TIMESTAMP    NOT NULL DEFAULT now(),
-    creator_id UUID         NOT NULL,
-    properties JSONB
+    CONSTRAINT fk_task_user
+        FOREIGN KEY (creator_id) REFERENCES app_user (id)
 );
 
--- 외래 키 (AppUser 테이블이 존재한다고 가정)
-ALTER TABLE task
-    ADD CONSTRAINT fk_task_user
-        FOREIGN KEY (creator_id) REFERENCES app_user (id);
+insert into category(id,
+                     name,
+                     creator_id)
+values ('00000000-0000-0000-0000-000000000001', '분류1', '00000000-0000-0000-0000-000000000001');
+
+-- task
+CREATE TABLE task
+(
+    id           UUID PRIMARY KEY,
+    parent_id    UUID,
+    seq          INTEGER,
+    title        VARCHAR(255) NOT NULL,
+    is_completed BOOLEAN               default false,
+    category_id  UUID,
+    start_at     TIMESTAMP,
+    due_at       TIMESTAMP,
+    priority     VARCHAR(20),
+    created_at   TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at   TIMESTAMP    NOT NULL DEFAULT now(),
+    creator_id   UUID         NOT NULL,
+    CONSTRAINT fk_task_user
+        FOREIGN KEY (creator_id) REFERENCES app_user (id),
+    CONSTRAINT fk_task_parent
+        FOREIGN KEY (parent_id) REFERENCES task (id)
+);
+
+-- task detail
+CREATE TABLE task_detail
+(
+    task_id    UUID PRIMARY KEY,
+    content    TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    CONSTRAINT fk_task_detail_task FOREIGN KEY (task_id) REFERENCES task (id)
+);
